0ca83f314442631869c8aef41a560067f6f9c827
alvacode <alvaro89_piura@hotmail.com>
2024-11-04 18:30:58 -0500

    ya

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ app.add_middleware(
 
 if __name__ == "__main__":
     uvicorn.run(
-        'app:app',
-        host="0.0.0.0",
-        port=8080
+        'main:app',
+        host='0.0.0.0',
+        port=8000
     )
 
 
