9943336cc66a2db5a210a8c990c6e701ad74be5a
alvacode <alvaro89_piura@hotmail.com>
2024-11-04 17:38:02 -0500

    gunicorn add

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ app.add_middleware(
 if __name__ == "__main__":
     uvicorn.run(
         'main:app',
-        reload=True,
-        port=8000
+        host="0.0.0.0",
+        port=8080
     )
 
 
